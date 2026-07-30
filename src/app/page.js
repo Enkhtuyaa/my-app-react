376f041c79fe1063c956414625eba87f1a562440
Enkhtuya Davaajav <enhtuya7880@gmail.com>
2026-07-30 12:48:44 +0800

    improved

--- src/app/page.js
+++ src/app/page.js
@@ -3,26 +3,94 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [text, setText] = useState(" ");
+  const [state, setState] = useState("All");
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+  const handleAddButtonClick = () => {
+    // inputValue "" bol aldaanii msg haruulna
+    if (inputValue.trim() === "") {
+      setErrorMessage("please enter todo");
+      return;
+    }
+    const newTodo = {
+      id: Date.now(),
+      title: inputValue,
+      status: "Active",
+      isDone: false,
+    };
+
+    // console.log("newTodo: ", newTodo);
+    // console.log("button clicked")
+    setTodos([...todos, newTodo]);
+
+    setInputValue("");
+  };
+  console.log(todos, "hello");
+  // console.log("this is  the todo", todos);
+  const handleActiveButtonClick = () => {
+    setState("Active");
+  };
+  const handleCompletedButtonClick = () => {
+    setState("Completed");
+  };
+  const handleAllButtonClick = () => {
+    setState("All");
+  };
+  // console.log(state, "this is the state");
+  // console.log(handleActiveButtonClick)
   return (
     <div className="container">
       <main className="todolist-card">
-         <h1 className="title">To-Do-list</h1>
-          <header className="container">
-            <input className="search-input" placeholder="Add a new task..." />
-            <button className="add-button">Add</button>
-           </header>
-               <section className="section">
-                  <button className="all-button">All</button>
-                  <button className="active-button">Active</button>
-                  <button className="completed-button">Completed</button>
-                </section>
-                   <h2 className="todolist-text">No tasks yet. Add one above!</h2>
-                    <footer className="footer-text">
-                      <h3 className="powered-by-text">Powered by</h3>
-                      <h4 className="pinecone-text">Pinecone academy</h4>
-                     </footer>
+        <h1 className="title">To-Do-list</h1>
+        <header className="container">
+          <input
+            className="search-input"
+            placeholder="Add a new task..."
+            onChange={handleInputChange}
+            value={inputValue}
+          />
+          {errorMessage !== "" && <div>{errorMessage}</div>}
+          <button className="add-button" onClick={handleAddButtonClick}>
+            Add
+          </button>
+        </header>
+        <section className="section">
+          <button className="all-button" onClick={handleAllButtonClick}>
+            All
+          </button>
+          <button className="active-button" onClick={handleActiveButtonClick}>
+            Active
+          </button>
+          <button
+            className="completed-button"
+            onClick={handleCompletedButtonClick}
+          >
+            Completed
+          </button>
+        </section>
+        {/* {state === "All" && <div>State now is All</div>}
+        {state === "Active" && <div>State now is Active</div>}
+        {state === "Completed" && <div>State now is completed</div>} */}
+        {/* {todos.length === 0 ? (
+          <h2 className="todolist-text">No tasks yet. Add one above!</h2>
+        ) : (
+          <div className="newtodo">
+            {todos.map((todo) => (
+             <div></div> <li key={todo.id}>{todo.title}</li>
+            ))}
+          </div>
+        )} */}
+
+        <footer className="footer-text">
+          <h3 className="powered-by-text">Powered by</h3>
+          <h4 className="pinecone-text">Pinecone academy</h4>
+        </footer>
       </main>
     </div>
   );
--- src/app/todolist/page.js
+++ src/app/todolist/page.js
@@ -3,25 +3,13 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [text, setText] = useState(" ");
-
-  return ( 
-    <div className="container">
-      <main className="todolist-card">
-         <h1 className="title">To-Do-list</h1>
-            <header className="container">
-                <input className="search-input">Add a new task...</input>
-                <button className="add-button">Add</button>
-             </header>
-               <section className="section">
-                  <button className="all-button">All</button>
-                  <button className="active-button">Active</button>
-                  <button className="completed-button">Completed</button>
-              </section>
-                <h2 className="todolist-text">No tasks yet. Add one above!</h2>
-
-      </main>
-    </div>
-    
-  );
+  const [count, setCount] = useState(0);
+  const handleButtonClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div id="">
+      {count} this the state
+      <button onClick={handleButtonClick}> Changenpm state</button>
+    </div>);
 }
